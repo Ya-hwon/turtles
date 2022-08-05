85d8c062f117b19bb121e6f0b8be089831500cda
Yahwon <37835539+Ya-hwon@users.noreply.github.com>
2022-08-05 10:15:25 +0200

    + loadsstringloop in startup

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,8 +1,9 @@
 shell.setDir("/")
 local startup_handle = fs.open("startup.lua", "w")
 startup_handle.writeLine("shell.setPath(shell.path()..\":/pkg\")")
-
+startup_handle.writeLine("while true do\npcall(loadstring(io.read()))\nend")
 startup_handle.close()
+
 fs.makeDir("pkg")
 shell.setDir("/pkg")
 
